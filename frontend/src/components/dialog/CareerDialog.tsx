9e83dc7637b2346e38c5787d2713ce2cb199c81c
Jaime <jaimerod26@gmail.com>
2025-01-22 16:36:30 -0500

    add comment for divider "test"

--- frontend/src/components/dialog/CareerDialog.tsx
+++ frontend/src/components/dialog/CareerDialog.tsx
@@ -25,6 +25,7 @@ export default function CareerDialog({isOpen, handleClose} : CareerDialogProps) 
                 <DialogContentText>
                     Software Developer
                 </DialogContentText>
+            {/*TODO:  Remove this divider*/}
             </DialogContent>
         <Divider/>
         </Dialog>
