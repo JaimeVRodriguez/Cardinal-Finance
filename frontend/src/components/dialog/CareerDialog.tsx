61647ebcdf62e490cbdef367cddb09e35583a91f
Jaime <jaime@Jaimes-MacBook-Pro.local>
2025-01-22 16:34:14 -0500

    add second divider

--- frontend/src/components/dialog/CareerDialog.tsx
+++ frontend/src/components/dialog/CareerDialog.tsx
@@ -26,6 +26,7 @@ export default function CareerDialog({isOpen, handleClose} : CareerDialogProps) 
                     Software Developer
                 </DialogContentText>
             </DialogContent>
+        <Divider/>
         </Dialog>
         </>
     )
